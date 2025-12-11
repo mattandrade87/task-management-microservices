1912d4622181996a515dbc75c0334d9facbf068c
mattandrade87 <mateus.vitor.andrade@gmail.com>
2025-12-11 15:24:34 -0300

    chore(auth-service): configura bootstrap do NestJS, habilita CORS e define porta do Auth Service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+
+  const port = process.env.PORT || 3002;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Auth Service is running on port ${port}`);
 }
 bootstrap();
