f2a14e89f03527a651e09dd76c5dcb3bb31a0c3a
mattandrade87 <mateus.vitor.andrade@gmail.com>
2025-12-12 16:21:25 -0300

    feat(api-gateway): simplificando app module

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'AUTH_SERVICE',
-        transport: Transport.TCP,
-        options: {
-          host: 'auth-service',
-          port: 3003,
-        },
-      },
-    ]),
-  ],
-  controllers: [AuthController],
+  imports: [AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
