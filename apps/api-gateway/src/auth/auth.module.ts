2c3f3c23c254e152efa3366e8219d4c536aa3aa7
mattandrade87 <mateus.vitor.andrade@gmail.com>
2025-12-12 16:21:01 -0300

    feat(api-gateway): configura módulo de autenticação com Passport, JWT e microserviço Auth

---
+++ apps/api-gateway/src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule,
+
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: 'auth-service',
+          port: 3003,
+        },
+      },
+    ]),
+  ],
+  controllers: [AuthController],
+  providers: [JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
+})
+export class AuthModule {}
